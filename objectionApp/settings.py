97cb0102cfc4c53bc3d32932953a4666fda6ebd6
Tope <temmytak@yahoo.co.uk>
2021-05-16 03:09:20 -0400

    ok

--- objectionApp/settings.py
+++ objectionApp/settings.py
@@ -141,6 +141,11 @@ LOGIN_REDIRECT_URL = "home-page"
 LOGIN_URL = "login-page"
 
 
+# SMTP Configuration
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 
 
 # SESSIONS
