f2f2ee7aab20bb087cd6e074cab29679a9fcdd16
Tope <temmytak@yahoo.co.uk>
2021-05-16 03:16:36 -0400

    ok

--- objectionApp/settings.py
+++ objectionApp/settings.py
@@ -146,7 +146,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-
+EMAIL_HOST_USER = 'databoidjango@gmail.com'
+EMAIL_HOST_PASSWORD = 'databoi1234%'
 
 # SESSIONS
 SESSION_COOKIE_AGE = 600
