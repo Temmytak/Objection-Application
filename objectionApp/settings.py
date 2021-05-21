4d07a588976befbdcc8311c141bcc7f3b8ee3680
Temmytak <49845048+Temmytak@users.noreply.github.com>
2021-05-21 19:34:25 -0400

    ok

--- objectionApp/settings.py
+++ objectionApp/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
+SECRET_KEY = 'django-insecure-ehviddt%g(f)(@m&cuk!nidr9gw$qasb35p-&2lrx#d)yo$x5d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
