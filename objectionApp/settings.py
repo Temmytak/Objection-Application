4eec3e44260aeb539eeb6031167154b793403e4a
Temmytak <49845048+Temmytak@users.noreply.github.com>
2021-05-21 19:28:10 -0400

    removed smtp details

--- objectionApp/settings.py
+++ objectionApp/settings.py
@@ -142,12 +142,7 @@ LOGIN_URL = "login-page"
 
 
 # SMTP Configuration
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'databoidjango@gmail.com'
-EMAIL_HOST_PASSWORD = 'databoi1234%'
+
 
 # SESSIONS
 SESSION_COOKIE_AGE = 600
